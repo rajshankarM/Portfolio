655cb4309b42b5bf343e5d3b4b25b238c6a4affe
RajshankarM <rajshankar607@gmail.com>
2025-02-18 10:59:46 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Container, Row, Button } from 'react-bootstrap';
-import { ThemeContext } from 'styled-components';
-import PropTypes from 'prop-types';
-import Fade from 'react-reveal/Fade';
-import Header from './Header';
-import endpoints from '../constants/endpoints';
-import ProjectCard from './projects/ProjectCard';
-import FallbackSpinner from './FallbackSpinner';
+import React, { useState, useEffect, useContext } from "react";
+import { Container, Row, Button } from "react-bootstrap";
+import { ThemeContext } from "styled-components";
+import PropTypes from "prop-types";
+// import Fade from 'react-reveal/Fade';
+import Header from "./Header";
+import endpoints from "../constants/endpoints";
+import ProjectCard from "./projects/ProjectCard";
+import FallbackSpinner from "./FallbackSpinner";
 
 const styles = {
   containerStyle: {
@@ -25,7 +25,7 @@ const Projects = (props) => {
 
   useEffect(() => {
     fetch(endpoints.projects, {
-      method: 'GET',
+      method: "GET",
     })
       .then((res) => res.json())
       .then((res) => setData(res))
@@ -35,31 +35,31 @@ const Projects = (props) => {
   return (
     <>
       <Header title={header} />
-      {data
-        ? (
-          <div className="section-content-container">
-            <Container style={styles.containerStyle}>
-              <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-                {data.projects?.slice(0, numberOfItems).map((project) => (
-                  <Fade key={project.title}>
-                    <ProjectCard project={project} />
-                  </Fade>
-                ))}
-              </Row>
+      {data ? (
+        <div className="section-content-container">
+          <Container style={styles.containerStyle}>
+            <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+              {data.projects?.slice(0, numberOfItems).map((project) => (
+                <div key={project.title}>
+                  <ProjectCard project={project} />
+                </div>
+              ))}
+            </Row>
 
-              {!showMore
-                && (
-                <Button
-                  style={styles.showMoreStyle}
-                  variant={theme.bsSecondaryVariant}
-                  onClick={() => setShowMore(true)}
-                >
-                  show more
-                </Button>
-                )}
-            </Container>
-          </div>
-        ) : <FallbackSpinner /> }
+            {!showMore && (
+              <Button
+                style={styles.showMoreStyle}
+                variant={theme.bsSecondaryVariant}
+                onClick={() => setShowMore(true)}
+              >
+                show more
+              </Button>
+            )}
+          </Container>
+        </div>
+      ) : (
+        <FallbackSpinner />
+      )}
     </>
   );
 };
